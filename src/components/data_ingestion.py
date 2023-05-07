f2219cadb974820ae78eda03f3634fca4cfee828
Juweriomohamed <juweriomohamedomar12345@gmail.com>
2023-05-07 10:37:14 +0300

    added dataset

---
+++ src/components/data_ingestion.py
@@ -0,0 +1,9 @@
+import os
+import sys
+import pandas as pd
+from src.exception import CustomException
+from src.logger import logging
+from sklearn.model_selection import train_test_split
+from dataclasses import dataclass
+
+@dataclass #use only when you defining variables
